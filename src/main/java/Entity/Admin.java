804d80a3a376cc53dc23f10ab86f7883b8c9cd0d
azizbo <azizbourguiba.ab@gmail.com>
2020-11-27 22:44:36 +0100

    classe profile

--- src/main/java/Entity/Admin.java
+++
@@ -1,5 +0,0 @@
-package Entity;
-
-public class Admin extends Personne {
-    private int id ;
-}
--- src/main/java/Entity/Personne.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-public abstract class Personne {
-    private int age;
-    private String nom,prenom,adresse,email;
-
-
-
-}
